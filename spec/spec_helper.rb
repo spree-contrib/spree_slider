f3c26b6a313840645499d4028408459640123c1a
Elias Fröhner <froehner@comcard.de>
2019-11-19 15:24:50 +0100

    active_storage_validations are required by engine

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ end
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../dummy/config/environment.rb', __FILE__)
+require File.expand_path('dummy/config/environment.rb', __dir__)
 
 require 'rspec/rails'
 require 'rspec/mocks'

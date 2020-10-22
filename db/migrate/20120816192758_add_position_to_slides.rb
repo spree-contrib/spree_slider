14019dd170825bdea5a6f644e728317675bba141
Prakash Poudel <prakash@truemark.com.np>
2020-10-22 18:37:24 +0545

    Fix comments

--- db/migrate/20120816192758_add_position_to_slides.rb
+++ db/migrate/20120816192758_add_position_to_slides.rb
@@ -1,5 +1,5 @@
 class AddPositionToSlides < ActiveRecord::Migration[6.0]
   def change
-    add_column :spree_slides, :position, :integer, :null => false, :default => 0
+    add_column :spree_slides, :position, :integer, null: false, default: 0
   end
 end
